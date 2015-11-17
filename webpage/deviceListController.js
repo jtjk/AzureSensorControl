bfc8e249ba43058cfdad4162830964fe27421a00
Janne Koskinen <jtjk@iki.fi>
2015-11-17 17:17:32 +0200

    showing devices as boxes

--- webpage/deviceListController.js
+++ webpage/deviceListController.js
@@ -1,4 +1,30 @@
 (function() {
+	// create the module and name it scotchApp
+        // also include ngRoute for all our routing needs
+    var SensorControlApp = angular.module('SensorControlApp', ['ngRoute']);
+
+    // configure our routes
+    SensorControlApp.config(function($routeProvider, $locationProvider) {
+        $routeProvider
+
+            // route for the home page
+            .when('/', {
+                templateUrl : 'main.html',
+                controller  : 'DevCtrl'
+            })
+
+            // route for the about page
+            .when('/device', {
+                templateUrl : 'devices.html',
+                controller  : 'DevCtrl'
+            });
+
+              // use the HTML5 History API
+        $locationProvider.html5Mode(true);
+    });
+
+
+
 	angular.module('SensorControlApp', []).controller('DevCtrl', function($scope, $http) {
 		$http.get('http://localhost:3000/devices').
 			success(function(data,status,headers,config) {
@@ -15,4 +41,8 @@
 	    ];
 */	    
 	});
+
+
+
+
 })();
